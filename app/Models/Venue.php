f52a4c983abc518e93be0ea43e328a2907f6f319
ShurGith <75266693+ShurGith@users.noreply.github.com>
2025-01-01 11:57:20 +0100

    Inicio

---
+++ app/Models/Venue.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Venue extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'city',
+        'country',
+        'postal_code',
+    ];
+
+    protected $casts = [
+        'id' => 'integer',
+    ];
+
+    public function conferences(): HasMany
+    {
+        return $this->hasMany(Conference::class);
+    }
+}
